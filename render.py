0ae376fdb2af52be4fd123817444d7f52e54d462
Lingxiao Zhang <823951506@qq.com>
2020-01-21 01:24:47 +0800

    keyshot render v1.1

--- render.py
+++ render.py
@@ -5,14 +5,15 @@ def cleanExt(ext):
         ext = ext[1:]
     return ext
 def main():
-    values = [("folder", lux.DIALOG_FOLDER, "Folder to import from:", None),
-              ("output", lux.DIALOG_FOLDER, "Folder to outut:", None),
+    values = [("folder", lux.DIALOG_FOLDER, "Folder to import from:", "D:/tsdmnet/test"),
+              ("output", lux.DIALOG_FOLDER, "Folder to outut:", "D:/tsdmnet/test"),
               ("iext", lux.DIALOG_TEXT, "Input file format to read:", "obj"),
               ("oext", lux.DIALOG_TEXT, "Output image format:", "png"),
               ("width", lux.DIALOG_INTEGER, "Output width:", 1920),
               ("height", lux.DIALOG_INTEGER, "Output height:", 1080),
-              ("time", lux.DIALOG_INTEGER, "render time second:", 90),
-              ("num", lux.DIALOG_INTEGER, "number of image:", 200)
+              ("time", lux.DIALOG_INTEGER, "render time second:", 1),
+              ("dis", lux.DIALOG_INTEGER, "distance:", 3),
+              ("num", lux.DIALOG_INTEGER, "number of image:", 1)
               ]
 
     opts = lux.getInputDialog(title = "Render",
@@ -49,27 +50,27 @@ def main():
     time = opts["time"]
     num = opts["num"]
     output_path = opts['output']
-
+    dis = opts["dis"]
     idx = 0
     env = lux.getActiveEnvironment()
-    lux.setSphericalCamera(90,0,0)
-    lux.setCameraDistance(3)
-    lux.setCameraLookAt(pt=(0,0.45,0))
     env.setBackgroundColor((255,255,255))
     opts = lux.getRenderOptions()
     opts.setMaxTimeRendering(time)
 
     for f in [f for f in os.listdir(fld) if f.endswith(iext)]:
-        path = fld + os.path.sep + f
+        path = fld + '/' + f
         opp = lux.getImportOptions()
         opp['retain_materials']=True
         opp['separate_materials']=True
-        lux.importFile(path, showOpts = False,opts = opp)
-
+        lux.importFile(path, showOpts = False, opts = opp)
         root = lux.getSceneTree()
-        # print(f[:-4])
         obj=root.find(name = f[:-4])[0]
-        # idx = idx + 1
+        lux.setSphericalCamera(90,0,0)
+        lux.setCameraDistance(dis)
+        lux.setCameraLookAt(pt=(0,0.45,0))
+        
+        
+        
 
         lux.renderImage(output_path+"/"+f[:-4]+'_'+str(0)+'.png', width = 1920, height = 1080, opts = opts)
         for i in range(1,num+1):
